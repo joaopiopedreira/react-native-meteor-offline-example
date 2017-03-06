9809a40596641e06882b2af527b477da7e71b613
jwdobken <dobken004@gmail.com>
2017-03-06 20:49:27 +0100

    allow all client-side updates

--- meteor/imports/api/links/links.js
+++ meteor/imports/api/links/links.js
@@ -3,3 +3,10 @@
 import { Mongo } from 'meteor/mongo';
 
 export const Links = new Mongo.Collection('links');
+
+// Allow all client-side updates
+Links.allow({
+  insert() { return true; },
+  update() { return true; },
+  remove() { return true; },
+});
