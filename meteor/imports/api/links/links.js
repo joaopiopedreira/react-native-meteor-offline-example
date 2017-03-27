ab11c811fbf840ebaff0d12f396432da193e0399
jwdobken <dobken004@gmail.com>
2017-03-27 15:01:20 +0200

    define collectionName first

--- meteor/imports/api/links/links.js
+++ meteor/imports/api/links/links.js
@@ -1,8 +1,20 @@
 // Definition of the links collection
 
-import { Mongo } from 'meteor/mongo';
+import OfflineCollectionVersions from '../../api/offlineCollections/offlineCollectionVersions';
+import OfflineMongo from '../../api/offlineCollections/collectionTransformer';
+import { Random } from 'meteor/random';
+// import { Mongo } from 'meteor/mongo';
 
-export const Links = new Mongo.Collection('links');
+const collectionName = 'links';
+
+if (!OfflineCollectionVersions.findOne({ collection: collectionName })) {
+  OfflineCollectionVersions.insert({
+    collection: collectionName,
+    offlineVersion: Random.id()
+  });
+}
+
+export const Links = new Mongo.Collection(collectionName);
 
 // Allow all client-side updates
 Links.allow({
